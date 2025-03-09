28b1be219353e9ec5059382142a9b5057dc150c7
Fædon Jóhannes <phaedonsinis@gmail.com>
2025-03-09 12:11:29 +0000

    Split VanillaOption into a separate header

--- derivatives/derivative.h
+++ derivatives/derivative.h
@@ -4,56 +4,9 @@
 #include "absl/log/log.h"
 #include "rates/rates_curve.h"
 #include "trees/binomial_tree.h"
+#include "vanilla_option.h"
 
 namespace markets {
-
-enum class OptionPayoff { Call, Put };
-enum class ExerciseStyle { European, American };
-
-struct VanillaOption {
-  VanillaOption(double strike,
-                OptionPayoff payoff,
-                ExerciseStyle style = ExerciseStyle::European)
-      : strike_(strike), payoff_(payoff), style_(style) {}
-
-  double getPayoff(double state, double strike) const {
-    const double dist_from_strike =
-        payoff_ == OptionPayoff::Call ? state - strike_ : strike_ - state;
-    return std::max(0.0, dist_from_strike);
-  }
-
-  double operator()(const BinomialTree& deriv_tree,
-                    const BinomialTree& asset_tree,
-                    int ti,
-                    int i,
-                    int ti_final,
-                    double up_prob,
-                    double fwd_df) const {
-    if (ti == ti_final) {
-      const double state = asset_tree.nodeValue(ti, i);
-      return getPayoff(state, strike_);
-    }
-
-    const double up = deriv_tree.nodeValue(ti + 1, i + 1);
-    const double down = deriv_tree.nodeValue(ti + 1, i);
-    const double down_prob = 1 - up_prob;
-
-    const double discounted_expected_next_state =
-        fwd_df * (up * up_prob + down * down_prob);
-
-    if (style_ == ExerciseStyle::American) {
-      const double state = asset_tree.nodeValue(ti, i);
-      const double payoff_if_early_exercise = getPayoff(state, strike_);
-      return std::max(payoff_if_early_exercise, discounted_expected_next_state);
-    }
-
-    return discounted_expected_next_state;
-  }
-
-  double strike_;
-  OptionPayoff payoff_;
-  ExerciseStyle style_;
-};
 
 class Derivative {
  public:
