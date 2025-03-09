7b6022e4a751b382c4e4144260e08dd290e472aa
Fædon Jóhannes <phaedonsinis@gmail.com>
2025-03-09 20:11:01 +0000

    Add default to switch statements to satisfy GCC

--- rates/curve_calculators.h
+++ rates/curve_calculators.h
@@ -10,13 +10,14 @@ namespace smileexplorer {
 inline double dfByPeriod(double r, double dt, CompoundingPeriod period) {
   const int p = static_cast<int>(period);
   switch (period) {
-    case CompoundingPeriod::kContinuous:
-      return std::exp(-r * dt);
     case CompoundingPeriod::kAnnual:
     case CompoundingPeriod::kSemi:
     case CompoundingPeriod::kQuarterly:
     case CompoundingPeriod::kMonthly:
       return 1. / std::pow(1 + r / p, dt * p);
+    case CompoundingPeriod::kContinuous:
+    default:
+      return std::exp(-r * dt);
   }
 }
 
@@ -27,13 +28,14 @@ inline double fwdRateByPeriod(double df_start,
   double df_ratio = df_start / df_end;
   const int p = static_cast<int>(period);
   switch (period) {
-    case CompoundingPeriod::kContinuous:
-      return std::log(df_ratio) / dt;
     case CompoundingPeriod::kAnnual:
     case CompoundingPeriod::kSemi:
     case CompoundingPeriod::kQuarterly:
     case CompoundingPeriod::kMonthly:
       return p * (std::pow(df_ratio, 1. / (p * dt)) - 1);
+    case CompoundingPeriod::kContinuous:
+    default:
+      return std::log(df_ratio) / dt;
   }
 }
 
