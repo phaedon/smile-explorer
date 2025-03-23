d5d69cd3ff7443e15d32e947098f8717637ffc1b
Fædon Jóhannes <phaedonsinis@gmail.com>
2025-03-23 19:07:58 +0000

    Reuse backward induction method

--- rates/fixed_cashflow_instrument.h
+++ rates/fixed_cashflow_instrument.h
@@ -108,29 +108,18 @@ class FloatingCashflowInstrument {
     }
   }
 
+  const ShortRateTreeCurve& shortRateModel() const {
+    return *short_rate_curve_;
+  }
+
   double price() {
-    // Subtract 2 because we need to start the backward induction one step
-    // before the end of the timegrid (since we always look one step forward).
-    const auto& short_rate_tree = short_rate_curve_->trinomialTree();
-
-    const int final_time_index = bond_tree_.tree_.size() - 2;
-    for (int ti = final_time_index; ti >= 0; --ti) {
-      for (int i = 0; i < bond_tree_.numStatesAt(ti); ++i) {
-        auto& curr_bond_node = bond_tree_.tree_[ti][i];
-        const auto& next_bond_nodes =
-            bond_tree_.getSuccessorNodes(curr_bond_node, ti, i);
-
-        double expected_next =
-            next_bond_nodes.up.state_value * curr_bond_node.branch_probs.pu +
-            next_bond_nodes.mid.state_value * curr_bond_node.branch_probs.pm +
-            next_bond_nodes.down.state_value * curr_bond_node.branch_probs.pd;
-
-        double r = short_rate_tree.shortRate(ti, i);
-
-        curr_bond_node.state_value +=
-            std::exp(-r * short_rate_tree.getTimegrid().dt(ti)) * expected_next;
-      }
+    auto status = runBackwardInduction(
+        shortRateModel(), bond_tree_, bond_tree_.tree_.size() - 1, 0);
+    if (!status.ok()) {
+      LOG(ERROR) << status.message();
+      return 0.0;
     }
+
     return bond_tree_.tree_[0][0].state_value;
   }
 
